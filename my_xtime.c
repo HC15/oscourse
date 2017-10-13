87c74a160bc355181661fdb1bf87b6dd3e1e03d4
root <root@CS350-hchen76.csvb.local>
2017-10-12 22:20:52 -0400

    Updated my_xtime.c

--- my_xtime.c
+++ my_xtime.c
@@ -5,9 +5,10 @@
 #include <linux/slab.h>
 
 asmlinkage int sys_my_xtime(struct timespec *current_time) {
-	if(access_ok(VERIFY_WRITE, current_time, sizeof(current_time))) {
-		*current_time = current_kernel_time();
-		printk(KERN_DEFAULT "Current time in nanoseconds: %lu", current_time->tv_nsec);
+	if(access_ok(VERIFY_WRITE, current_time, sizeof(*current_time))) {
+		struct timespec kernel_time = current_kernel_time();
+		current_time = &kernel_time;
+		printk(KERN_DEFAULT "Current time in nanoseconds: %ld", current_time->tv_nsec);
 		return 0;
 	}
 	else{
