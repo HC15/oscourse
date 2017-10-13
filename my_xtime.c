16fe964443e3ae95f826e1814ae85a03f2ae3cb3
root <root@CS350-hchen76.csvb.local>
2017-10-12 20:43:18 -0400

    Added system call of my_xtime

---
+++ my_xtime.c
@@ -0,0 +1,16 @@
+#include <linux/linkage.h>
+#include <linux/time.h>
+#include <asm/uaccess.h>
+#include <linux/printk.h>
+#include <linux/slab.h>
+
+asmlinkage int sys_my_xtime(struct timespec *current_time) {
+	if(access_ok(VERIFY_WRITE, current_time, sizeof(current_time))) {
+		*current_time = current_kernel_time();
+		printk(KERN_DEFAULT "Current time in nanoseconds: %lu", current_time->tv_nsec);
+		return 0;
+	}
+	else{
+		return -EFAULT;
+	}
+}
