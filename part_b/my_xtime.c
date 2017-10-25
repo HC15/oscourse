ca3b46bacb464eecb33ad9cfdd820aab6222fe6e
root <root@CS350-hchen76.csvb.local>
2017-10-25 18:49:32 -0400

    Added part B folder

---
+++ part_b/my_xtime.c
@@ -0,0 +1,19 @@
+#include <linux/linkage.h>
+#include <linux/time.h>
+#include <asm/uaccess.h>
+#include <linux/printk.h>
+#include <linux/slab.h>
+
+asmlinkage int sys_my_xtime(struct timespec *current_time) {
+	if(access_ok(VERIFY_WRITE, current_time, sizeof(*current_time))) {
+		struct timespec kernel_time = current_kernel_time();
+		copy_to_user(current_time, &kernel_time, sizeof(kernel_time));
+		printk(KERN_DEFAULT "Current time in nanoseconds: %ld", current_time->tv_nsec);
+		return 0;
+	}
+	else{
+		return -EFAULT;
+	}
+}
+
+EXPORT_SYMBOL(sys_my_xtime);
