0133d337dcea1b7c3060527e10320830e0621363
root <root@CS350-hchen76.csvb.local>
2017-10-27 08:16:35 -0400

    Updated mytime.c with getnstimeofday

--- part_c/mytime.c
+++ part_c/mytime.c
@@ -7,34 +7,47 @@
 #include <linux/time.h>
 #include <asm/uaccess.h>
 
-MODULE_LICENSE("DUAL BSD/GPL");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Harvey Chen");
 
 #define NAME "mytime"
 static int deviceNumber;
 
 static int mytime_open(struct inode *, struct file *);
-static ssize_t mytime_read(struct file *file, char __user * out, size_t size, loff_t * off);
+static int mytime_close(struct inode *, struct file *);
+static ssize_t mytime_read(struct file *, char __user *, size_t, loff_t *);
 
 static struct file_operations my_fops = {
+	.owner = THIS_MODULE,
 	.open = mytime_open,
+	.release = mytime_close,
 	.read = mytime_read
 };
 
 static int __init mytime_init(void) {
 	printk(KERN_ALERT "mytime module init\n");
 	deviceNumber = register_chrdev(0, NAME, &my_fops);
+	if(deviceNumber < 0) {
+		printk(KERN_ALERT "mytime module init failed\n");
+		return deviceNumber;
+	}
+	printk(KERN_ALERT "mytime module init success\n");
 	return 0;
 }
 
 static int mytime_open(struct inode *inode, struct file *file) {
-	printk(KERN_ALERT "mytime device opened");
+	printk(KERN_ALERT "mytime device opened\n");
+	return 0;
+}
+
+static int mytime_close(struct inode *inode, struct file *file) {
+	printk(KERN_ALERT "mytime device closed\n");
 	return 0;
 }
 
 static ssize_t mytime_read(struct file *file, char __user * out, size_t size, loff_t * off) {
 	if(access_ok(VERIFY_WRITE, out, size)) {
-		char* buffer = (char*) kmalloc(sizeof(char) * 75, GFP_KERNEL);
+		char* buffer = (char*) kmalloc(sizeof(char) * size, GFP_KERNEL);
 
 		int success;
 		success = sprintf(buffer, "current_kernel_time: ");
@@ -52,13 +65,27 @@ static ssize_t mytime_read(struct file *file, char __user * out, size_t size, lo
 		success += sprintf(buffer + success, kernel_time_sec);
 		kfree(kernel_time_sec);
 
+		success += sprintf(buffer + success, "\n");
 
 		struct timespec timeofday;
 		getnstimeofday(&timeofday);
 
+		length = snprintf(NULL, 0, "%ld", timeofday.tv_nsec);
+		char* timeofday_nsec = (char*) kmalloc(length + 1, GFP_KERNEL);
+		success += sprintf(buffer + success, timeofday_nsec);
+		kfree(timeofday_nsec);
+
+		length = snprintf(NULL, 0, "%ld", timeofday.tv_sec);
+		char* timeofday_sec = (char*) kmalloc(length + 1, GFP_KERNEL);
+		success += sprintf(buffer + success, timeofday_sec);
+		kfree(timeofday_sec);
+
+		success += sprintf(buffer + success, "\n");
+
 		printk(KERN_DEFAULT "current_kernel_time: %ld %ld \ngetnstimeofday: %ld %ld", kernel_time.tv_sec, kernel_time.tv_nsec, timeofday.tv_sec, timeofday.tv_nsec);
+
 		copy_to_user(out, &buffer, size);
-		return 1;
+		return success;
 	}
 	else{
 		return -EFAULT;
@@ -72,3 +99,4 @@ static void __exit mytime_exit(void) {
 
 module_init(mytime_init);
 module_exit(mytime_exit);
+
