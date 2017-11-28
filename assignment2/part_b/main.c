8b01d65787cd20f764adb7f84ba03d20cd7c7b03
root <root@CS350-hchen76.csvb.local>
2017-11-28 11:45:03 -0500

    Moved assignment2 files to on folder to prepare for assignment 3

---
+++ assignment2/part_b/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/unistd.h>
+#include <linux/time.h>
+
+#define _NR_sys_my_xtime 326
+
+int main() {
+	long success;
+	struct timespec ts;
+	success = syscall(_NR_sys_my_xtime, &ts);
+	if(success == 0) {
+		printf("Current time in seconds: %ld\nCurrent time in nanoseconds: %ld\n", ts.tv_sec, ts.tv_nsec);
+	}
+	printf("syscall return value: %ld\n", success); // 0 means call successful
+	return 0;
+}
